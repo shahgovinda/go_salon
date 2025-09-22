d10d9a71426d4e8bc8211a82480441b19f7b9a47
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-09-22 16:51:46 +0530

    Update page.tsx

--- app/beauticians/page.tsx
+++ app/beauticians/page.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 
 const beauticians = [
   {
-    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmVtYWxlJTIwcG9ydHJhaXR8ZW58MHx8MHx8fDA%3D",
+    image: "/priya.jpg",
     name: "Yelena Belova",
     description: "\"Expert in modern hair styling and color treatments.\"",
     skill: "Hair Stylist"
